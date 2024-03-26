63333f3a0f8b4f19fd77021fdcf2ddc2d2ba48a3
Echezona Oji <echyth@yahoo.com>
2024-03-26 11:28:45 +1000

    feat(task 4): add a script that prepares mysql test server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- script that prepares a MySQL server for the project:
+
+-- A database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- A new user hbnb_test (in localhost)
+-- The password of hbnb_test should be set to hbnb_test_pwd
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
