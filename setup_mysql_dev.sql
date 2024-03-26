3894405410e5d4c5b7793bc8913965f9a64444c2
Echezona Oji <echyth@yahoo.com>
2024-03-26 11:24:54 +1000

    feat(task 3): script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- script that prepares a MySQL server for the project:
+
+-- A database hbnb_dev_db if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- A new user hbnb_dev (in localhost)
+-- The password of hbnb_dev should be set to hbnb_dev_pwd
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
