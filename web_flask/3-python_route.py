1351f2cad75e8ce096ec8b1118166bb2e87b4ea3
Echezona Oji <echyth@yahoo.com>
2024-04-09 22:04:34 +1000

    feat(task 3): initial commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
